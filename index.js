87e28c64e380e5538a26824f473de98086717472
S.P. ROY <sprithi44@gmail.com>
2023-03-07 15:20:33 -0500

    inquirer prompt for logo characters added

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const { Triangle, Circle, Square } = require("./lib/shapes");
 const fs = require("fs");
 
 const shapes = [Triangle, Circle, Square];
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "Enter up to three characters for the logo:",
+      validate: (input) =>
+        input.length <= 3 ? true : "Please enter up to three characters",
+    },
+  ]);
