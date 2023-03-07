5db8869d9c9c43d3078df23bfb4066db07a15d0d
S.P. ROY <sprithi44@gmail.com>
2023-03-07 15:40:26 -0500

    prompt for text color, shape and shape color added

--- index.js
+++ index.js
@@ -14,4 +14,24 @@ inquirer
       validate: (input) =>
         input.length <= 3 ? true : "Please enter up to three characters",
     },
+    {
+      type: "input",
+      name: "textColor",
+      message:
+        "Enter the text color as a CSS color keyword or hexadecimal color value:",
+      default: "black",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Select a shape for the logo:",
+      choices: shapes.map((shape) => shape.name),
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message:
+        "Enter the shape color as a CSS color keyword or hexadecimal color value:",
+      default: "white",
+    },
   ]);
